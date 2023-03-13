5aebcf0ed9d3c1d37ab1add66e6247af72319868
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-03-13 23:09:26 +0300

    Done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves (dwarves)
+    dwarves.map.with_index { |dwarves, index| puts "#{index + 1}. #{dwarves}"}
+  end
+
+def summon_captain_planet planeteer_calls
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls.map{ |planeteer_calls| planeteer_calls.capitalize + "!" }
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls calls_long
+  length = calls_long.find do |call|
+    call.size > 4 
+  end
+  return length ? true : false
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese snacks
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+    snacks.find {|snack| cheese_types.include? snack} 
 end
